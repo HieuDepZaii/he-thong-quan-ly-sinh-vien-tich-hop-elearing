b97b7bc05d2669e24bd6b8bdbe2a66b933041df7
HieuDepZaii <davidhieu1234@gmail.com>
2021-05-22 16:49:57 +0700

    add giangvien route
    thêm route giảng viên cho zoom controller

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Http;
 
+use App\Http\Middleware\GiangVien;
 use App\Http\Middleware\IsAdmin;
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
 
@@ -63,6 +64,7 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-        'admin'=>IsAdmin::class
+        'admin'=>IsAdmin::class,
+        'giang-vien'=>GiangVien::class,
     ];
 }
